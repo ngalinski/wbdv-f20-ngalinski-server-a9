68248c8fe5c42d804ef76033863ec428f1f54d2a
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-12-04 21:59:52 -0800

    working through cloud db

--- server.js
+++ server.js
@@ -2,30 +2,31 @@
 
 const express = require('express')
 const app = express()
+
+var cors = require('cors');
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+app.use(
+    cors({
+             credentials: true,
+             origin: true
+         })
+);
+app.options('*', cors());
+
 // TODO: use mongoose
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/whiteboard',
+mongoose.connect('mongodb+srv://ngalinski:WebDevF2020@cluster0.xmdhr.mongodb.net/whiteboard?retryWrites=true&w=majority',
                  {useNewUrlParser: true, useUnifiedTopology: true});
 
-//mongoose.connect(process.env.MONGODB_URL,
-//                 {useNewUrlParser: true,
-//                     useUnifiedTopology: true});
-
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers',
-               'Content-Type, X-Requested-With, Origin');
-    res.header('Access-Control-Allow-Methods',
-               'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    next();
-});
 
 require('./controllers/quizzes.controller.server')(app)
 require('./controllers/questions.controller.server')(app)
 require('./controllers/quiz-attempts.controller.server')(app)
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () =>
+    console.log("Server started!")
+);
